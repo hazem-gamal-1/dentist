887091fec8423ea37124077980c92abf945b26fb
Hazem Gamal <111183931+hazem-gamal-1@users.noreply.github.com>
2025-05-02 01:28:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ config = Config()
 
 # Initialize model
 model = DentalModel(num_classes=config.NUM_CLASSES, model_name="efficientnet_b2")
-model.load_state_dict(torch.load(model_path, map_location=device))
+model.load_state_dict(torch.load(model_path, map_location=device, weights_only=False))
+
 model.to(device)
 model.eval()
 
